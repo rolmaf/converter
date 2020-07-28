86bf90fa18e03f902c2ab424a9c126827ad54ed8
rolmaf <61619288+rolmaf@users.noreply.github.com>
2020-07-28 23:10:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function transferCurrence() {
+    let currentValue1 = document.querySelector(".current-1__currency").value;
+    let currentValue2 = document.querySelector(".current-2__currency").value;
+    let amountCurrentValue1 = document.querySelector(".current-1__amount").value;
+    let xhr = new XMLHttpRequest();
+
+    let url = `https://api.exchangerate-api.com/v4/latest/${currentValue1}`;
+
+    xhr.open("GET", url, true);
+
+    xhr.send();
+
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            let res = JSON.parse(xhr.response);
+            console.log(res);
+            document.querySelector(".current-2__amount").value = res.rates[currentValue2] * amountCurrentValue1;
+
+        };
+    };
+
+}
+
+
+
+document.querySelector(".current-1__amount").addEventListener("input", transferCurrence);
